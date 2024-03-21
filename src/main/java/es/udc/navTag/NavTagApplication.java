1eddae15ef0a411e02b2721a553a12dac8f3ff03
David Fernandez Calvelo <d.fernandez4@udc.es>
2024-03-21 01:53:48 +0100

    Initial Project Structure

---
+++ src/main/java/es/udc/navTag/NavTagApplication.java
@@ -0,0 +1,13 @@
+package es.udc.navTag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavTagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/udc/navTag/NavTagApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.navTag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
