db7fdaad98bfb17cf80a4de75efad6c1592c46d7
David Fernandez Calvelo <d.fernandez4@udc.es>
2024-04-16 01:58:04 +0200

    Added DAOs

---
+++ src/main/java/es/udc/navTag/model/entities/UserDao.java
@@ -0,0 +1,28 @@
+package es.udc.navTag.model.entities;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The Interface UserDao.
+ */
+public interface UserDao extends JpaRepository<User,Long>{
+    
+    /**
+	 * Exists by user name.
+	 *
+	 * @param userName the user name
+	 * @return true, if successful
+	 */
+	boolean existsByUserName(String userName);
+
+	/**
+	 * Find by user name.
+	 *
+	 * @param userName the user name
+	 * @return the optional
+	 */
+	Optional<User> findByUserName(String userName);
+
+}
