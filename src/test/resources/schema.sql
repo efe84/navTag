5f58a0c69546c70e47ba3fc19930882235ce807f
David Fernandez Calvelo <d.fernandez4@udc.es>
2024-04-16 16:36:21 +0200

    Creation of testDB

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -37,19 +37,3 @@ CREATE TABLE Command (
     time TIMESTAMP,
     FOREIGN KEY (tag_id) REFERENCES Tag(id)
 );
-
-INSERT INTO User (username, name, email, password, version) VALUES
-('user1', 'user one', 'user1@example.com', 'password1', 0),
-('user1', 'user two', 'user2@example.com', 'password2', 0);
-
-INSERT INTO Tag (user_id, name, description) VALUES
-(1, 'Tag1', 'Description of Tag 1'),
-(2, 'Tag2', 'Description of Tag 2');
-
-INSERT INTO Location (tag_id, latitude, longitude, time) VALUES
-(1, 40.7128, -74.0060, NOW()),
-(2, 34.0522, -118.2437, NOW());
-
-INSERT INTO Command (tag_id, type, state, time) VALUES
-(1, 'Type1', 'State1', NOW()),
-(2, 'Type2', 'State2', NOW());
